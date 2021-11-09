f3407aa0871c965f7a4cfdfb781bfd5ff95ceaa6
AxelGrape <41511588+AxelGrape@users.noreply.github.com>
2021-11-09 01:26:49 -0800

    Updated interface

--- python_interface/client_comm.py
+++ python_interface/client_comm.py
@@ -1,10 +1,13 @@
 
 import subprocess
+import file_handler
 
 def get_request(file_name):
     program_name = "./hq"
     hq_mode = "-mode=client"
     file_path = "--path=/" + file_name
     storage_path = "-outdir=/home/axel/Downloads/"
-    boll = subprocess.run([program_name, hq_mode, file_path, storage_path])
-    print(boll)
+    subprocess.run([program_name, hq_mode, file_path, storage_path])
+
+def custom_get_request(params):
+    subprocess.run(params)
