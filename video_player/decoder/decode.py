701145e8d0fb279d89d36ac912b83bd04ebccdae
Benjamin Salomonsson <benjamin.salo@gmail.com>
2021-11-15 07:12:54 -0800

    update decoder

--- video_player/decoder/decode.py
+++ video_player/decoder/decode.py
@@ -1,24 +1,39 @@
 import os
+import subprocess
 def decoder(path, si, ei, quality):
     quality1 = str(int(quality) + 1)
 
+    if(len(si.lstrip('0')) == 0):
+        return False, 'Invalid startindex'
+        
     #rnge = [*range(int(si.lstrip('0')), int(ei.lstrip('0')))]
     chnks = [str(item).zfill(len(ei)) for item in [*range(int(si.lstrip('0')), int(ei.lstrip('0'))+1)]]
 
     print("remove contents of out? (y/n)")
     #if input() == "y":
-    os.system("rm out/*")
+    subprocess.run("rm out/*", shell = True, check = True)
 
 
 #os.system( "cat $(ls -vx sample_video/dash_init_*.m4s) > sampleF.mp4")
 #os.system( "cat $(ls -vx sample_video/dash_chunk_*.m4s) >> sampleF.mp4")
+    if(os.path.isfile(path + "/dash_init_" + quality + ".m4s") & os.path.isfile(path + "/dash_init_" + quality1 + ".m4s")):
+        try:
+            subprocess.run( "cat " + path + "/dash_init_" + quality + ".m4s > out/all" + quality + ".m4s", shell = True, check = True)
+            subprocess.run( "cat " + path + "/dash_init_" + quality1 + ".m4s > out/all" + quality1 + ".m4s", shell = True, check = True)
+        except subprocess.CalledProcessError:
+            print("OPS")
 
-    os.system( "cat " + path + "/dash_init_" + quality + ".m4s > out/all" + quality + ".m4s")
-    os.system( "cat " + path + "/dash_init_" + quality1 + ".m4s > out/all" + quality1 + ".m4s")
-    for c in chnks:
-        os.system( "cat $(ls -vx " + path + "/dash_chunk_" + quality + "_"+str(c)+".m4s) >> out/all" + quality + ".m4s")
-        os.system( "cat $(ls -vx " + path + "/dash_chunk_" + quality1 + "_"+str(c)+".m4s) >> out/all" + quality1 + ".m4s")
-
+    
+        for c in chnks:
+            if(os.path.isfile(path + "/dash_chunk_" + quality + "_"+str(c)+".m4s") & os.path.isfile(path + "/dash_chunk_" + quality1 + "_"+str(c)+".m4s")):
+                print(f'c is {c}')
+                try:
+                    subprocess.run( "cat $(ls -vx " + path + "/dash_chunk_" + quality + "_"+str(c)+".m4s) >> out/all" + quality + ".m4s", shell = True, check = True)
+                    subprocess.run( "cat $(ls -vx " + path + "/dash_chunk_" + quality1 + "_"+str(c)+".m4s) >> out/all" + quality1 + ".m4s", shell = True, check = True)
+                except subprocess.CalledProcessError:
+                    print("O.P.S.I.E" + str(c))
+            else:
+                return False, f'chunk creation failed: {c}'
 
 
 
@@ -31,25 +46,31 @@ def decoder(path, si, ei, quality):
     #os.system( "cat SampleVideo_1280x720_10mb/dash_init_1.m4s $(ls -vx SampleVideo_1280x720_10mb/dash_chunk_1_0000[4-7].m4s) > out/all1_.m4s")
 
     #os.system("ffmpeg -i out/all5.m4s -c copy out/Vid5.mp4")
-    os.system("ffmpeg -i out/all" + quality + ".m4s -i out/all" + quality1 + ".m4s -c:v copy -c:a aac out/vid" + ei + ".mp4")
+        subprocess.run("ffmpeg -i out/all" + quality + ".m4s -i out/all" + quality1 + ".m4s -c:v copy -c:a aac out/vid" + ei + ".mp4", shell = True, check = True)
     #os.system("ffmpeg -i out/all0_.m4s -i out/all1_.m4s -c:v copy -c:a aac out/all01_.mp4")
-
-    print("Play vid? (y/n)")
-    if input() == "y":
-        os.system("xdg-open out/vid" + ei + ".mp4")
+        return True, "out/vid" + ei + ".mp4"
+        
 
     #os.system("printf \"file '%s'\n\" out/*.mp4 > mylist.txt")
     #os.system("ffmpeg -f concat -i mylist.txt -c copy out/tmp.mp4")
     #os.system("ffmpeg -i out/tmp.mp4 -c copy out/all01.mp4 -y")
     #echo \"y\" | 
     #os.system("xdg-open out/all01.mp4")
+    else:
+        return False, 'Init file failed'
 
 def main():
-    path = 'SampleVideo_1280x720_10mb'
+    path = 'nature'
     startindex = '00001'
     slutindex = '00007'
     quality = '0'
-    decoder(path, startindex, slutindex, quality)
+    siri = decoder(path, startindex, slutindex, quality)
+
+    print("Playing video")
+    if siri[0]:
+        subprocess.run("xdg-open " + siri[1], shell = True, check = True)
+    else:
+        print(siri[1])
 
 if __name__ == "__main__":
     main()
