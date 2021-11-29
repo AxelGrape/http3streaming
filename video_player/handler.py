a4f5de50f9a8713cbdeadb1939a7c4ba710bf0da
Benjamin Salomonsson <benjamin.salo@gmail.com>
2021-11-29 06:11:31 -0800

    worke on handler

--- video_player/handler.py
+++ video_player/handler.py
@@ -47,6 +47,7 @@ class RunHandler:
     #PRE: Path to downloaded .mpd file
     #POST: parser object
     def parse_mpd(self):
+        self.mpdPath = '/home/benjamin/Desktop/DVAE08/http3streaming/video_player/vid/nature/dash.mpd'
         try:
             self.parsedObj = MPDParser(self.mpdPath)
         except:
@@ -54,27 +55,31 @@ class RunHandler:
             return False, "Failed to get parser object"
             
             
+   
+    
 
-    #PRE: .mpd file has been parsed, got parser object, Index of current chunk(eg. 00009)
-    #POST: List of next segment(s)
-    def next_segment():
-        print("no")
-
-    #PRE: List of next segments
+    #PRE: parser object
     #POST: path to next chunks(dir), Startindex, endindex, quality
-    def parse_segment_list(segmentList):
+    def parse_segment(self):
+        q = 0
+        segment = self.parsedObj.get_next_segment(q)
+        vidPath = self.mpdPath.replace("dash.mpd", "")
+        try:
+            index = segment[0][-9:-4]
+            quality = segment[0][-11:-10]
+        except: 
+            print("wops")
+        
+        print(index)
+        print(quality)    
+        print(vidPath)
         print("no")
+        request_file(segment[0])
+        request_file(segment[1])
+        decode_segments(vidPath, index, index, quality)
 
-    #PRE: path to next chunks(dir), Index of start and end chunk, quality
-    #POST: path to .mp4 file
-    def decode_segments(path, si, ei, q):
-        success,mp4Path = decode_segment(path, si, ei, q)#(bool, pathToMp4File)
-        if success :
-            print("Path is: " + mp4Path)
-            #continue with stuff
-        else:
-            print("Error: " + mp4Path)
-            #handle fault stuff
+
+    
 
 
     #PRE: 
@@ -82,3 +87,28 @@ class RunHandler:
     #decides when new segments(chunks) should be sent to videoplayer
     def queue_handler():
         print("no")
+
+
+#PRE: path to next chunks(dir), Index of start and end chunk, quality
+#POST: path to .mp4 file
+def decode_segments(path, si, ei, q):
+    success,mp4Path = decode_segment(path, si, ei, q)#(bool, pathToMp4File)
+    if success :
+        print("Path is: " + mp4Path)
+        #continue with stuff
+    else:
+        print("Error: " + mp4Path)
+        #handle fault stuff
+
+def main():
+    Handler = RunHandler()
+    print("hej")
+    p = MPDParser('/home/benjamin/Desktop/DVAE08/http3streaming/video_player/vid/nature/dash.mpd')
+    a = p.get_next_segment(0)
+    print(a)
+    print(p.get_segment_duration(a[0]))
+    Handler.parse_mpd()
+    Handler.parse_segment()
+
+if __name__ == "__main__":
+    main()
